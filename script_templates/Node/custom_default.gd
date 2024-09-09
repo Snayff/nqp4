aa07bb4b10f9e1d635a497a332d60d436ee1d153
Snayff <23128268+Snayff@users.noreply.github.com>
2024-09-09 11:05:59 +0100

    docs/initial_docs (#1)
    create initial guides for style, architecture, and developer.

---
+++ script_templates/Node/custom_default.gd
@@ -0,0 +1,42 @@
+# meta-description: Default, generic template.
+# meta-default: true
+## class desc
+#@icon("")
+#class_name XXX
+extends _BASE_
+
+
+#region SIGNALS
+
+#endregion
+
+
+#region ON READY (for direct children only)
+
+#endregion
+
+
+#region EXPORTS
+# @export_group("Component Links")
+# @export var 
+#
+# @export_group("Details")
+#endregion
+
+
+#region VARS
+
+#endregion
+
+
+#region FUNCS
+
+
+
+
+
+
+
+
+
+#endregion
