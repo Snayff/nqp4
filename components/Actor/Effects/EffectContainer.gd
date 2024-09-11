3d49c06e9808987ddd9d9a6323a2bb2f6c811265
Maxime Blade <gabel.maxime.blade@gmail.com>
2024-09-11 11:14:39 +0200

    Added EffectContainer class

---
+++ components/Actor/Effects/EffectContainer.gd
@@ -0,0 +1,31 @@
+extends Effect
+
+@export var effects: Array[Effect] = []
+
+func initialize(_target: Actor) -> void:
+    target = _target
+    for effect in effects:
+        effect.initialize(target)
+    target.effects_changed.emit()
+
+func execute() -> void:
+    for effect in effects:
+        effect.execute()
+
+func execute_process(delta: float) -> void:
+    for effect in effects:
+        effect.execute_process(delta)
+
+## Used on an effect's physics process event
+# @virtual
+func execute_physics_process(delta: float) -> void:
+    for effect in effects:
+        effect.execute_physics_process(delta)
+
+## Optionally used when this effect is no longer relevant.
+## Used for self-cleanup
+# @virtual
+func destroy() -> void:
+    super()
+    for effect in effects:
+        effect.destroy()
